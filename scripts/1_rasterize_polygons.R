88ff3bb9babfb9af0916a7a72beca502d737822b
Christy Rollinson <crollinson@gmail.com>
2017-05-12 15:59:23 -0500

    Save files showing polygons of tree cover;
    Also start script about rasterizing polygons into a grid to look at %
    cover

---
+++ scripts/1_rasterize_polygons.R
@@ -0,0 +1,5 @@
+# Formatting Tree Cover Data to make comparisons easier.
+#
+# Steps
+# 1. Rasterize forest layers from 1850, 1939, & 2010 --> make percent "forest ecosystem" or "tree" cover
+# 2. 
--- scripts/exploratory.R
+++ scripts/exploratory.R
@@ -1,5 +1,5 @@
 library(rgeos); library(rgdal)
-
+library(raster)
 
 setwd("~/Desktop/Research/Chicago_Urban_Forest_Origins/data/")
 fgdb <- "OriginsData.gdb" # Note: file paths do NOT work --> must be in the directory you're trying to pull it from
@@ -20,8 +20,25 @@ summary(forest.2010)
 canopy.grid <- readOGR(fgdb, "CanopyForestGrid")
 class(canopy.grid)
 summary(canopy.grid)
-plot(canopy.grid)
+# canopy.grid <- spTransform(canopy.grid, proj4string(forest.1850))
 dim(canopy.grid)
+
+png("../figures/TreeCover_1850.png", height=8, width=5, units="in", res=220)
+plot(forest.1850, col="green3", lwd=0.1, border="green3")
+plot(spTransform(canopy.grid, proj4string(forest.1850)), add=T, lwd=0.25)
+dev.off()
+
+
+png("../figures/TreeCover_1939.png", height=8, width=5, units="in", res=220)
+plot(forest.1939, col="green3", lwd=0.1, border="green3")
+plot(spTransform(canopy.grid, proj4string(forest.1939)), add=T, lwd=0.25)
+dev.off()
+
+png("../figures/TreeCover_2010.png", height=8, width=5, units="in", res=220)
+plot(forest.2010, col="green3", lwd=0.1, border="green3")
+plot(spTransform(canopy.grid, proj4string(forest.2010)), add=T, lwd=0.25)
+dev.off()
+
 
 test <- coordinates(canopy.grid)
 class(test); dim(test)
