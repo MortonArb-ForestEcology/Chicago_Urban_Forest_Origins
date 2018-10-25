0891f19958378884140b88bef4a66137aea774dd
Christy Rollinson <crollinson@gmail.com>
2018-10-25 13:27:23 -0500

    Export new PalEON Biomass product as a spatial grid for Linsday

---
+++ scripts/paleon_biomass.R
@@ -0,0 +1,32 @@
+# Convert PalEON Biomass into Arc-Friendly spatial grids
+
+path.drive <- "/Volumes/GoogleDrive/My Drive/OriginsSources-CRTI"
+dat.nc <- ncdf4::nc_open(file.path(path.drive, "analyses/data/PalEON_Biomass/PLS_biomass_western_point_v0.999.nc"))
+
+dat.nc$var$Total$longname
+
+bm.total <- ncdf4::ncvar_get(dat.nc, "Total")
+paleon.x <- ncdf4::ncvar_get(dat.nc, "x")
+paleon.y <- ncdf4::ncvar_get(dat.nc, "y")
+
+bm.df <- data.frame(x=rep(paleon.x, length(paleon.y)), 
+                    y=rep(paleon.y, each=length(paleon.x)), 
+                    Biomass=stack(data.frame(bm.total))[,1])
+
+library(ggplot2)
+png("/Volumes/GoogleDrive/My Drive/OriginsSources-CRTI/analyses/figures/Biomass_PLS.png", height=8, width=8, unit="in", res=320)
+ggplot(data=bm.df) + coord_equal(expand=0) +
+  geom_raster(aes(x=x, y=y, fill=Biomass))
+dev.off()
+
+library(sp); library(rgdal); library(raster)
+bm.sp <- SpatialPixelsDataFrame(points=bm.df[,c("x", "y")], data=data.frame(bm.df[,c("Biomass")]), proj4string = CRS("+init=epsg:3175"))
+
+png(file.path(path.drive, "analyses/figures/Biomass_PLS2.png"), height=8, width=8, unit="in", res=320)
+plot(bm.sp)
+dev.off()
+
+bm.sp <- raster(bm.sp)
+names(bm.sp) <- "Biomass-Total"
+
+writeRaster(bm.sp, filename=file.path(path.drive, "analyses/data/PalEON_Biomass/PLS_biomass_western_point_v0.999_TOTAL"), overwrite=T)
